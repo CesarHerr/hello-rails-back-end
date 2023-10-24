b15ec0678221008079d96a4cb8547cf13bafaecb
Cesar Herrera <cesarherr321@gmail.com>
2023-10-24 13:30:57 -0300

    Add and config rack-cors

--- config/application.rb
+++ config/application.rb
@@ -13,12 +13,12 @@ module HelloRailsBackEnd
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'http://localhost:3001'
+        resource '*', headers: :any, methods: [:get, :post, :put, :delete, :options]
+      end
+    end
   end
 end
+
