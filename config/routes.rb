0d53aa0bd4bd013abd85217df9ebcf22e15f0dc2
Cesar Herrera <cesarherr321@gmail.com>
2023-10-24 12:26:46 -0300

    Add controller for the API

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  namespace :api do
+    get 'greetings/index'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
