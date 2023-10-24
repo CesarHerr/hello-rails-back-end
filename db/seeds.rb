1fc8b25c5193c306cdb1cccaf5fa03b77eed35d6
Cesar Herrera <cesarherr321@gmail.com>
2023-10-24 15:06:23 -0300

    Add seeds to add greetings to the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Greeting.create(greetings: 'Hello, how are you today?')
+Greeting.create(greetings: "Hi there, it's great to see you again!")
+Greeting.create(greetings: "Good morning, I hope you have a wonderful day ahead!")
+Greeting.create(greetings: "Good afternoon, I trust you're having a productive day.")
+Greeting.create(greetings: "Good evening, I wanted to wish you a relaxing night.")
