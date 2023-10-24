4b7e2bded3a58d39b9fa3be48bb4c94293a96ad4
Cesar Herrera <cesarherr321@gmail.com>
2023-10-24 12:33:10 -0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  namespace :api do
-    get 'greetings/index'
+
+namespace :api do
+  namespace :v1 do
+    resources :greetings, only: [:index]
   end
+end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
